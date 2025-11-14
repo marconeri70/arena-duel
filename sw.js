ac897335db0092380279b84692b03760e4e56572
marconeri70 <marconeri70@gmail.com>
2025-11-14 19:06:31 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,12 @@
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('arena-duel-v2').then(cache => cache.addAll([
-      './',
-      './index.html',
-      './manifest.webmanifest',
-      './sw.js'
-    ]))
-  );
+  self.skipWaiting();
 });
+
+self.addEventListener('activate', event => {
+  clients.claim();
+});
+
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+  // Lascia passare tutte le richieste in rete (no cache complessa)
+  return;
 });
